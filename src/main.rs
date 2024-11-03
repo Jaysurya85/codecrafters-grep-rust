70fc767df5c388194b8095a5a2f28afbcf0ee661
Jaysurya85 <jaysuryaray1998@gmail.com>
2024-11-03 05:41:35 +0530

    changes for digits

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,7 @@ use std::io;
 use std::process;
 
 fn match_pattern(input_line: &str, pattern: &str) -> bool {
-    if pattern.chars().count() == 1 {
-        return input_line.contains(pattern);
-    } else {
-        panic!("Unhandled pattern: {}", pattern)
-    }
+    input_line.contains(pattern)
 }
 
 // Usage: echo <input_text> | your_program.sh -E <pattern>
